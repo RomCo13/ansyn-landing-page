ec39d9670d2456a227553e5ed1b832855764cc97
rom <romcohen115@gmail.com>
2020-08-11 09:09:15 +0300

    adding home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ImagesComponent } from './images/images.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule,MatIconModule } from'@angular/material'
+
+
+const appRoutes:Routes=[
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home' , component:HomepageComponent},
+  {path:'presention' , component:ImagesComponent},
+];
 
 @NgModule({
   declarations: [
@@ -18,6 +26,7 @@ import { MatButtonModule,MatIconModule } from'@angular/material'
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
